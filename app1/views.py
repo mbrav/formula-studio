9e1fb693b1accc9218c03d0cb765ca8a00f6a788
mbrav <mbrav@protonmail.com>
2020-10-20 21:55:09 +0300

    added list with databse id

--- app1/views.py
+++ app1/views.py
@@ -7,7 +7,10 @@ class Another(View):
 
     books = Book.objects.all()
 
-    output = f"We have {len(books)} books"
+    output = ''
+
+    for book in books:
+        output += f"We have {book.title} book in DB with ID {book.id} <br>"
 
     def get(self, request):
         return HttpResponse(self.output)
