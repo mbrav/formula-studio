24377e9b635c39d227f294161a51341b0377ec2c
mbrav <mbrav@protonmail.com>
2020-10-22 02:27:49 +0300

    one to one databse relation

--- app1/models.py
+++ app1/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+class BookNumber(models.Model):
+    sibn_10 = models.CharField(max_length=10, blank=True)
+    sibn_13 = models.CharField(max_length=13, blank=True)
+
 class Book(models.Model):
     STATUS = (
         (0,'Unknown'),
@@ -20,6 +24,8 @@ class Book(models.Model):
 
     published = models.DateField(auto_now=True, blank=True, null=True)
     is_published = models.BooleanField(default=False)
+
+    number = models.OneToOneField(BookNumber, null=True, blank=True, on_delete=models.CASCADE)
 
     # cover = models.FileField(upload_to='covers/')
     cover = models.ImageField(upload_to='covers/', blank=True)
--- app1/serializers.py
+++ app1/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import Book
+from .models import BookNumber
+
+class BookNumberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BookNumber
+        fields = ['id', 'sibn_10', 'sibn_13']
 
 class BookSerializer(serializers.ModelSerializer):
+    number = BookNumberSerializer(many = False)
     class Meta:
         model = Book
-        fields = ['id', 'title', 'author', 'description', 'price']
+        fields = ['id', 'title', 'author', 'description', 'price', 'number']
