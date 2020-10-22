dea54395e01a166a04b0a1f49bb7cb248afde585
mbrav <mbrav@protonmail.com>
2020-10-22 16:10:29 +0300

    many to many relation

--- app1/models.py
+++ app1/models.py
@@ -16,8 +16,6 @@ class Book(models.Model):
 
     title = models.CharField(max_length=36, blank=False, unique=True, default='')
 
-    author = models.CharField(max_length=36, blank=False, unique=True, default='')
-
     description = models.TextField(max_length=256, blank=True)
 
     price = models.DecimalField(default=0, decimal_places=2, max_digits=10, blank=True)
@@ -34,3 +32,8 @@ class Book(models.Model):
 class Character(models.Model):
     name = models.CharField(max_length=30)
     book = models.ForeignKey(Book, on_delete = models.CASCADE, related_name='characters')
+
+class Author(models.Model):
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+    books = models.ManyToManyField(Book, related_name='authors')
