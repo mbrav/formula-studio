c24c40024b6dffd9c01a57c7dcfb5737b0820fa3
mbrav <mbrav@protonmail.com>
2020-10-22 03:28:42 +0300

    one to many database relation

--- app1/admin.py
+++ app1/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Book
-from .models import BookNumber
+from .models import Book, BookNumber, Character
 
 # Register your models here.
 
 admin.site.register(Book)
 admin.site.register(BookNumber)
+admin.site.register(Character)
--- app1/serializers.py
+++ app1/serializers.py
@@ -1,14 +1,19 @@
 from rest_framework import serializers
-from .models import Book
-from .models import BookNumber
+from .models import Book, BookNumber, Character
 
 class BookNumberSerializer(serializers.ModelSerializer):
     class Meta:
         model = BookNumber
         fields = ['id', 'sibn_10', 'sibn_13']
 
+class CharacterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Character
+        fields = ['id', 'name']
+
 class BookSerializer(serializers.ModelSerializer):
     number = BookNumberSerializer(many = False)
+    characters = CharacterSerializer(many = True)
     class Meta:
         model = Book
-        fields = ['id', 'title', 'author', 'description', 'price', 'number']
+        fields = ['id', 'title', 'author', 'description', 'price', 'number', 'characters']
