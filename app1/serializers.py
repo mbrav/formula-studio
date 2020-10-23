a48029df47f41e25554a6f003a6b794e193f947a
mbrav <mbrav@protonmail.com>
2020-10-23 03:17:50 +0300

    I did not sign up for this

--- app1/serializers.py
+++ app1/serializers.py
@@ -4,16 +4,27 @@ from .models import Member, Subscription, SubscriptionType
 class SubscriptionTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = SubscriptionType
-        fields = ['id', 'name', 'price', 'subscription_type']
+        fields = ['id', 'name', 'price', 'description', 'subscription_type']
 
 class SubscriptionSerializer(serializers.ModelSerializer):
-    typeof = SubscriptionTypeSerializer(many = False)
+    subscription_type = SubscriptionTypeSerializer(many = False)
     class Meta:
         model = Subscription
-        fields = ['id', 'registration_date','typeof', 'fee_status']
+        fields = ['id', 
+                  'member', 
+                #   'registration_date',
+                  'subscription_type', 
+                  'fee_status']
 
 class MemberSerializer(serializers.ModelSerializer):
     subscriptions = SubscriptionSerializer(many = True)
     class Meta:
         model = Member
-        fields = ['id', 'first_name', 'last_name', 'mobile_number', 'email', 'subscriptions', 'description', 'registered_on']
+        fields = ['id', 
+                  'first_name',
+                  'last_name', 
+                  'mobile_number', 
+                  'email',
+                  'subscriptions',
+                #   'registered_on',
+                  'description']
