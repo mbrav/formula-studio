842311d391d98e306245837f5e40abbd0909c1b8
mbrav <mbrav@protonmail.com>
2020-10-23 16:48:41 +0300

    added payment model

--- app1/admin.py
+++ app1/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Member, Subscription, SubscriptionType
+from .models import Member, Payment, Subscription, SubscriptionType
+
 # Register your models here.
 
 admin.site.register(Member)
+admin.site.register(Payment)
 admin.site.register(Subscription)
 admin.site.register(SubscriptionType)
--- app1/serializers.py
+++ app1/serializers.py
@@ -1,29 +1,45 @@
 from rest_framework import serializers
-from .models import Member, Subscription, SubscriptionType
+from .models import Member, Payment, Subscription, SubscriptionType
 
 class SubscriptionTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = SubscriptionType
         fields = ['id', 'name', 'price', 'description']
 
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = [
+            'id', 
+            'payment_date', 
+            'payment_amount', 
+            'payment_type', 
+            'payment_paid',
+            'member'
+        ]
+
+
 class SubscriptionSerializer(serializers.ModelSerializer):
+    payment = PaymentSerializer(many = False)
     class Meta:
         model = Subscription
-        fields = ['id', 
-                #   'registration_date',
-                  'member', 
-                #   'fee_status',
-                  'subscription_type']
+        fields = ['id',
+            #   'registration_date',
+            'member',
+            'subscription_type',
+            'payment'
+        ]
 
 class MemberSerializer(serializers.ModelSerializer):
     subscriptions = SubscriptionSerializer(many = True)
     class Meta:
         model = Member
         fields = ['id', 
-                  'first_name',
-                  'last_name', 
-                  'mobile_number', 
-                  'email',
-                #   'registered_on',
-                  'subscriptions',
-                  'description']
+            'first_name',
+            'last_name',
+            'mobile_number',
+            'email',
+            #   'registered_on',
+            'subscriptions',
+            'description',
+        ]
