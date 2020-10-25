448965eb92a644f10429828435001dd6272d2115
mbrav <mbrav@protonmail.com>
2020-10-26 01:59:47 +0300

    Working mess

--- api/models.py
+++ api/models.py
@@ -13,6 +13,10 @@ class Member(models.Model):
 
     image = models.ImageField(upload_to='img/member_profiles', blank=True)
 
+    class Meta:
+        verbose_name = 'Member'
+        verbose_name_plural = 'Members'
+
     def __str__(self):
         return "%s %s" % (self.first_name, self.last_name)
 
@@ -28,7 +32,7 @@ class Payment(models.Model):
         ('2', 'Card'),
     )
 
-    payment_date = models.DateField()
+    payment_date = models.DateField(auto_now_add=True)
 
     payment_amount = models.DecimalField(
         default=0, 
@@ -47,53 +51,132 @@ class Payment(models.Model):
         choices=PAYMENT_PAID, 
     )
 
-    member = models.ForeignKey('Member', on_delete=models.CASCADE)
+    member = models.ForeignKey('Member', 
+        on_delete = models.CASCADE,
+        related_name='payments',
+        help_text='Payments that member has made'
+    )
+
+    class Meta:
+        verbose_name = 'Payment'
+        verbose_name_plural = 'Payments'
+        ordering = ('-payment_date',)
 
     def __str__(self):
         return "#%s – (%s)" % (self.id, self.payment_date)
 
-class SingleVisit(models.Model):
-    name = models.CharField(max_length=30)
-    
-    payment = models.OneToOneField(Payment, null=False, blank=False, on_delete=models.CASCADE)
-
-    member = models.ForeignKey('Member', 
-        on_delete = models.CASCADE,
-        related_name='single_visits',
-        help_text='Member to which this single visit is assigned'
-    )
-
-    def __str__(self):
-        return "%s" % (self.name)
+# TODO: ADD ENCASHMENT MODEL
 
 class SubscriptionType(models.Model):
     name = models.CharField(max_length=30)
-    description = models.CharField(('Subscription'), max_length=300, blank=True, default='description...')
+    description = models.CharField(('Description'), max_length=300, blank=True, default='description...')
+    number_of_visits = models.PositiveIntegerField(blank=False, null=False)
+    validity_in_days = models.PositiveIntegerField(blank=False, null=False)
+
+    class Meta:
+        verbose_name = 'Subscription type'
+        verbose_name_plural = 'Subscription types'
+        ordering = ('-name',)
 
     def __str__(self):
         return "%s" % (self.name)
 
-class Subscription(models.Model):
-    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular subscription")
-    registration_date = models.DateField()
+class Subscription(Payment):
+    registration_date = models.DateField(auto_now_add=True)
     description = models.CharField(('Description'), max_length=300, blank=True, default='description...')
 
-    payment = models.OneToOneField(Payment, null=False, blank=False, on_delete=models.CASCADE)
+    # TODO: ADD SUBSCRIPTION EXTENSION MODEL
 
-    member = models.ForeignKey('Member', 
-        on_delete = models.CASCADE,
-        related_name='subscriptions',
-        help_text='Member to which this subscription is assigned'
-    )
-
+    # THIS MUST BE ONE TO ONE
     subscription_type = models.ForeignKey('SubscriptionType',
         on_delete=models.CASCADE,
         blank=False,
         related_name='subscription_type',
-        help_text='Select a subscription type') 
+        help_text='Select a subscription type')
+
+    class Meta:
+         verbose_name = 'Subscription'
+         verbose_name_plural = 'Subscriptions'
+         ordering = ('-registration_date',)
+
 
     def __str__(self):
         return "#%s – (%s)" % (self.id, self.registration_date)
+
+class SingleVisit(Payment):
+    date = models.DateField(auto_now_add=True)
+    
+    # payment = models.OneToOneField('Payment', null=False, blank=False, on_delete=models.CASCADE)
+
+    # group = models.ForeignKey('Subscription',
+    #     on_delete=models.CASCADE,
+    #     blank=False,
+    #     related_name='visits',
+    #     help_text='A single visit)
+
+    class Meta:
+        verbose_name = 'Single visit'
+        verbose_name_plural = 'Single visits'
+        ordering = ('-date',)
+
+    def __str__(self):
+        return "%s" % (self.payment)
+
+class ItemPurchase(Payment):
+    date = models.DateField(auto_now_add=True)
+    name = models.CharField(max_length=30)
+
+    class Meta:
+        verbose_name = 'Visit'
+        verbose_name_plural = 'Visits'
+
+    def __str__(self):
+        return "%s" % (self.first_name, self.last.name)
+
+class GroupCategory():
+    name = models.CharField(max_length=30)
+    description = models.CharField(('Description'), max_length=300, blank=True, default='description...')
+
+    class Meta:
+        verbose_name = 'Group category'
+        verbose_name_plural = 'Group categories'
+
+
+    def __str__(self):
+        return "%s" % (self.name)
+
+class Group():
+    name = models.CharField(max_length=30)
+    category = models.OneToOneField('GroupCategory', null=False, blank=False, on_delete=models.CASCADE)
+
+    # TODO: ADD INSTRUCTOR / USER (?) MODEL
+
+    class Meta:
+        verbose_name = 'Group'
+        verbose_name_plural = 'Groups'
+
+    def __str__(self):
+        return "%s" % (self.name)
+
+# VISIT IS NOW SUBSCRIPTION VISIT - TO RENAME
+    
+class Visit(models.Model):
+    date = models.DateField()
+    # group = models.OneToOneField('Group', null=False, blank=False, on_delete=models.CASCADE)
+
+    subscription = models.ForeignKey('Subscription',
+        on_delete=models.CASCADE,
+        blank=False,
+        related_name='visits',
+        help_text='A visit based on a subscription')
+
+    class Meta:
+        verbose_name = 'Subscription visit'
+        verbose_name_plural = 'subscription visits'
+
+    def __str__(self):
+        return "%s" % (self.first_name, self.last.name)
+
 
 
 
