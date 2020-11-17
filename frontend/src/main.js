3e0c56945967bce97011ed95c0b1ee491969583e
mbrav <mbrav@protonmail.com>
2020-11-18 01:35:51 +0300

    "BootstrapVue not available for Vue3 and Bootstrap 5 yet" workaround

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,19 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// Dont import BootstrapVue for now
+// See https://github.com/bootstrap-vue/bootstrap-vue/issues/5196
+
+// import BootstrapVue from 'bootstrap-vue'
+import "bootstrap/dist/css/bootstrap.css";
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// TEMP, until BootstrapVue support for Vue3 is ready
+
+import "bootstrap/dist/js/bootstrap.bundle.js";
+
 const app = createApp(App);
 app.use(store);
 app.use(router);
+// app.use(BootstrapVue)
 app.mount("#app");
