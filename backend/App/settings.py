f688ca1948efb942b1136802d2a646efbabb595d
mbrav <mbrav@protonmail.com>
2021-10-29 12:43:31 +0300

    changes

--- backend/App/settings.py
+++ backend/App/settings.py
@@ -194,11 +194,16 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 # USER SETTINGS (modify at your own wish)
 
 USER_SETTINGS = {
-    'CURRENCY_SYMBOL': '₽'
+    'CURRENCY_SYMBOL': env('CURRENCY_SYMBOL')
 }
 
 GOOGLE_CALENDAR_ID = env('GOOGLE_CALENDAR_ID')
